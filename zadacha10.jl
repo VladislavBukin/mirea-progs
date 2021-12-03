91a6a43be97a89753fe5c8805126aa9a49429590
VladislavBukin <90093302+VladislavBukin@users.noreply.github.com>
2021-12-03 10:38:28 +0300

    Create zadacha10.jl

---
+++ zadacha10.jl
@@ -0,0 +1,34 @@
+function main(::Robot)
+  a = []
+  summa = 0 
+  side = Ost 
+  while !isborder(r, Nord)
+  movess!(r, side, a)
+    if side == Ost
+      side = West
+    else
+      side = Ost
+    end
+  end
+  movess!(r, side, a)
+  k = length(a)
+  while length(a) > 0 
+    summa += pop!(a)
+  end
+  println(summa/k)
+
+end
+
+
+function movess!(::Robot, side, a)
+  while !isborder(r, side)
+    if ismarker(r)
+      push!(a, temperature(r))
+    end
+    move!(r,side)
+  end
+  if !isborder(r, Nord)
+    move!(r, Nord)
+  end
+end
+
