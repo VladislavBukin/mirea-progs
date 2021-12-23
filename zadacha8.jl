7d15cf2266991e45da18d1d42703f8d05b7788ba
VladislavBukin <90093302+VladislavBukin@users.noreply.github.com>
2021-12-23 22:32:20 +0300

    Update zadacha8.jl

--- zadacha8.jl
+++ zadacha8.jl
@@ -1,16 +1,16 @@
 function main(::Robot)
   i = 1 
   while isborder(r, Nord)
-    movess!(r, i, West)
-    movess!(r, i, Ost)
-    movess!(r, i, Ost)
-    movess!(r, i, West)
+    move_to_the_side!(r, i, West)
+    move_to_the_side!(r, i, Ost)
+    move_to_the_side!(r, i, Ost)
+    move_to_the_side!(r, i, West)
     i += 1 
   end
 end
 
 
-function movess!(::Robot, j::Int, side::HorizonSide)
+function move_to_the_side!(::Robot, j::Int, side::HorizonSide)
   for i in 1:j
     if isborder(r,Nord)
       move!(r, side)
