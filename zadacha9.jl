bee6a99d86b3d5d0c769c6f4385332b39245c27a
VladislavBukin <90093302+VladislavBukin@users.noreply.github.com>
2021-12-23 22:35:44 +0300

    Update zadacha9.jl

--- zadacha9.jl
+++ zadacha9.jl
@@ -1,30 +1,30 @@
 function main(::Robot)
-  j = 2 
-  while !ismarker(r)
-    if !ismarker(r)
-      move!(r, Nord)
+    j = 2 
+    while !ismarker(r)
+      if !ismarker(r)
+        move!(r, Nord)
+      end
+      if !ismarker(r)
+        move!(r,West)
+      end
+      main_moves(r, j)
+      j += 2
+  
     end
-    if !ismarker(r)
-      move!(r,West)
-    end
-    movess!(r, j)
-    j += 2
-
-  end
 end
-
-
-function movess!(::Robot, j::Int)
-  help_movess!(r, j, Ost)
-  help_movess!(r, j, Sud)
-  help_movess!(r, j, West)
-  help_movess!(r, j, Nord)
+  
+  
+function main_moves!(::Robot, j::Int)
+    help_moves!(r, j, Ost)
+    help_moves!(r, j, Sud)
+    help_moves!(r, j, West)
+    help_moves!(r, j, Nord)
 end
-
-function help_movess!(::Robot, j::Int, side::HorizonSide)
-  for i in 1:j 
-    if !ismarker(r)
-      move!(r, side)
+  
+function help_moves!(::Robot, j::Int, side::HorizonSide)
+    for i in 1:j 
+      if !ismarker(r)
+        move!(r, side)
+      end
     end
-  end
 end
